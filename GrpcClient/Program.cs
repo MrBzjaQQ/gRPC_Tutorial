e9a7a2fbd780f413470810871115ad9e2e74c917
Илья Ершов <ershov.1ly4@gmail.com>
2022-07-15 18:03:57 +0600

    Tutorial is finished

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+// var input = new HelloRequest { Name = "UserName" };
+var clientRequested = new CustomerLookupModel { UserId = 2 };
+
+var channel = GrpcChannel.ForAddress("https://localhost:7230");
+//var client = new Greeter.GreeterClient(channel);
+
+//var reply = await client.SayHelloAsync(input);
+
+//Console.WriteLine(reply.Message);
+
+var customerClient = new Customer.CustomerClient(channel);
+
+var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+Console.WriteLine("New Customer List:");
+
+using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        Console.WriteLine($"{currentCustomer.FirstName} {currentCustomer.LastName} {currentCustomer.EmailAddress}");
+    }
+}
+
+Console.ReadKey();
---
+++ GrpcServer/Services/CustomersService.cs
@@ -0,0 +1,88 @@
+﻿using Grpc.Core;
+
+namespace GrpcServer.Services
+{
+    public class CustomersService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomersService> _logger;
+
+        public CustomersService(ILogger<CustomersService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel();
+
+            switch (request.UserId)
+            {
+                case 1:
+                    {
+                        output.FirstName = "Jamie";
+                        output.LastName = "Bright";
+                        break;
+                    }
+                case 2:
+                    {
+                        output.FirstName = "Alex";
+                        output.LastName = "Cooper";
+                        break;
+                    }
+                case 3:
+                    {
+                        output.FirstName = "Niels";
+                        output.LastName = "Brightbrained";
+                        break;
+                    }
+                default:
+                    {
+                        output.FirstName = "Default";
+                        output.LastName = "Customer";
+                        break;
+                    }
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(
+            NewCustomerRequest request,
+            IServerStreamWriter<CustomerModel> responseStream,
+            ServerCallContext context)
+        {
+            var customers = new List<CustomerModel> {
+                new CustomerModel
+                {
+                    FirstName = "Bilbo",
+                    LastName = "Baggins",
+                    Age = 80,
+                    EmailAddress = "bilbo.baggins@example.com",
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Max",
+                    LastName = "Maximoff",
+                    Age = 120,
+                    EmailAddress = "max.maximoff@example.com",
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FirstName = "Hello",
+                    LastName = "World",
+                    Age = 1,
+                    EmailAddress = "hello.world@example.com",
+                    IsAlive = false
+                }
+            };
+
+            foreach (var cust in customers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
